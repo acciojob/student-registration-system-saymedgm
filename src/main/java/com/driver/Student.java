eced9134fc8976099fbf02161af744d3590f7e71
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-25 17:23:59 +0530

    1

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -1,31 +1,28 @@
 package com.driver;
 
 public class Student {
-	private int studentId;
+    private int studentId;
     private String name;
 
     public Student(int studentId, String name) {
-        // your code goes here
+        this.studentId = studentId;
+        this.name = name;
     }
 
     // Getters and Setters
     public int getStudentId() {
-    	 // your code goes here
         return studentId;
     }
 
     public void setStudentId(int studentId) {
-    	 // your code goes here
         this.studentId = studentId;
     }
 
     public String getName() {
-    	 // your code goes here
         return name;
     }
 
     public void setName(String name) {
-    	 // your code goes here
         this.name = name;
     }
 }
--- src/main/java/com/driver/StudentRegistrationSystem.java
+++ src/main/java/com/driver/StudentRegistrationSystem.java
@@ -1,27 +1,34 @@
 package com.driver;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class StudentRegistrationSystem {
-	 private List<Student> studentList;
-	    private Set<Integer> registeredStudentIds;
+	private List<Student> studentList;
+	private Set<Integer> registeredStudentIds;
 
-	    public StudentRegistrationSystem() {
-	       // your code goes here
-	    }
+	public StudentRegistrationSystem() {
+		studentList = new ArrayList<>();
+		registeredStudentIds = new HashSet<>();
+	}
 
-	    public void registerStudent(Student student) {
-	        // your code goes here
-	    }
+	public void registerStudent(Student student) {
+		if (!registeredStudentIds.contains(student.getStudentId())) {
+			studentList.add(student);
+			registeredStudentIds.add(student.getStudentId());
+			System.out.println("Student registered successfully.");
+		} else {
+			System.out.println("Student with the same ID already exists.");
+		}
+	}
 
-	    public boolean isStudentRegistered(int studentId) {
-	    	//your code goes here
-	        return registeredStudentIds.contains(studentId);
-	    }
+	public boolean isStudentRegistered(int studentId) {
+		return registeredStudentIds.contains(studentId);
+	}
 
-	    public List<Student> getAllRegisteredStudents() {
-	    	// your code goes here
-	        return studentList;
-	    }
+	public List<Student> getAllRegisteredStudents() {
+		return studentList;
+	}
 }
